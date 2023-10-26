e9d97c2c016891ad3ac7d3fb396f2694930326e9
nblancoz <nblancozappala@gmail.com>
2023-10-26 13:38:52 +0200

    ejercicio 2 terminado

--- ejercicio2/ej2.js
+++ ejercicio2/ej2.js
@@ -12,6 +12,34 @@ app.get("/productos", (req, res) => {
     res.send("Listado de Productos")
 })
 
+app.post("/productos", (req, res) => {
+    res.send("Crear un producto")
+})
+
+app.put("/productos", (req, res) => {
+    res.send("Actualizar un producto")
+})
+
+app.delete("/productos", (req, res) => {
+    res.send("Borrar un producto")
+})
+
+app.get("/usuarios", (req, res) => {
+    res.send("Listado de usuarios")
+})
+
+app.post("/usuarios", (req, res) => {
+    res.send("Crear usuario")
+})
+
+app.put("/usuarios", (req, res) => {
+    res.send("Actualizar usuarios")
+})
+
+app.delete("/usuarios", (req, res) => {
+    res.send("Borrar un usuario")
+})
+
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
 });
