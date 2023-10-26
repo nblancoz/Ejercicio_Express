93211239b2d6d5b687d2b715de8279bab6611429
nblancoz <nblancozappala@gmail.com>
2023-10-26 13:30:11 +0200

    get añadidos en ej2

--- ejercicio2/ej2.js
+++ ejercicio2/ej2.js
@@ -4,7 +4,14 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Bienvenidos")
+app.get("/", (req, res) => {
+  res.send("Bienvenidos");
+});
+
+app.get("/productos", (req, res) => {
+    res.send("Listado de Productos")
 })
 
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
